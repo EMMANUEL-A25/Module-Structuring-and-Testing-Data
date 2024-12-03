babde6708321d06d69989993d187a3ece74207e0
Emmanuel <esiziba71@gmail.com>
2024-12-03 16:11:50 +0000

    getCardValue test cases

---
+++ Sprint-3/implement/get-card-value.test.js
@@ -0,0 +1,20 @@
+const getCardValue = require('./get-card-value')
+
+test('getCardValue returns 11 for Ace', () => {
+    expect(getCardValue('A♠')).toBe(11);
+  });
+
+
+test('getCardValue returns 10 for J, Q or K', () => {
+    expect(getCardValue('J, Q, K')).toBe(10);
+  });
+
+
+test('getCardValue returns parseInt(rank) for 2 & 9', () => {
+  expect(getCardValue('2')).toBe(parseInt('2'));
+  expect(getCardValue('9')).toBe(parseInt('9'));
+});
+
+test('getCardValue throws error for invalid rank', () => {
+  expect(() => getCardValue('invalid rank')).toThrow('Invalid card rank.');
+});
