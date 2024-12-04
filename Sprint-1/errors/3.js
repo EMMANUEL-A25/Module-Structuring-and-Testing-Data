5d6defaf9aed5c6645c640189f6b4bf801c28d42
Emmanuel <esiziba71@gmail.com>
2024-12-04 12:00:07 +0200

    Update 3.js
    spelling error

--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -2,19 +2,19 @@
 //const last4Digits = cardNumber.toString[0].slice(-4);
  
 
-// The last4Digits variable should store the last 4 digits of cardNumber
+// The last4Digits variable should store the last 4 digits of the card number
 // However, the code isn't working
 // Before running the code, make and explain a prediction about why the code won't work
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
-// Then try updating the expression last4Digits is assigned to, in order to get the correct value
+// Then try updating the expression last4Digits is assigned to, to get the correct value
 
 Prediction:
 //The code won't work because there was no console.log to show our output.
 
 //Actual error when the code is run: "Cannot read properties of undefined (reading 'slice')"
 
-//I think the reason it was giving us an error was because we did not give slice something to work with. We should have given slice an array or a string to get the correct output;
+//I think it was giving us an error because we did not give slice something to work with. We should have given the slice an array or a string to get the correct output;
 
 var cardNumber = ["4533787178994213"];
 const last4Digits = cardNumber[0].slice(-4);
