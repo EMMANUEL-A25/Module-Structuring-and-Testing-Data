bbac933d907ccbbe39c7b0828a29f9dcd70171a2
Emmanuel <esiziba71@gmail.com>
2024-12-04 07:32:00 +0000

    getOrdinalNumber function

---
+++ Sprint-3/revise/implement/get-ordinal-number.js
@@ -0,0 +1,9 @@
+function getOrdinalNumber(number) {
+    if (number === 1) {
+      return number + "st";
+    } else if (number === 2) {
+      return number + "nd";
+    } else if (number === 3) {
+      return number + "rd";
+    }  
+  }
