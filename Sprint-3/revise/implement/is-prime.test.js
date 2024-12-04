c5fbc334c588898930638b6c9ee4631e1922e97a
Emmanuel <esiziba71@gmail.com>
2024-12-04 07:47:30 +0000

    test cases for is prime function

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -1,3 +1,9 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+
+const isPrime = require('./is-prime');
+
+test('checks if 7 is a prime number', () => {
+    expect(isPrime(7)).toBe(true); 
+});
