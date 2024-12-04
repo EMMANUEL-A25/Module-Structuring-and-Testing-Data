586a99454e8b2aa2930c98cb77ae406e9be928e6
Emmanuel <esiziba71@gmail.com>
2024-12-04 11:58:34 +0200

    Update 1.js
    spelling errors

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -3,7 +3,7 @@
 //const age = 33;
 //age = age + 1;
 
-//The error is age is declared as a constant variable. which means it can not be reassigned to a new Value.
+//The error is that age is declared a constant variable, which means it can not be reassigned to a new Value.
 
 //fixing the   error:
 
