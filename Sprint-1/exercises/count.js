c13eb709502f711c9e3e159744aa6c9246f7ed7b
Emmanuel <esiziba71@gmail.com>
2024-12-04 12:00:50 +0200

    Update count.js
    spelling error

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -5,4 +5,4 @@ count = count + 1;
 //ANSWER: Line 3 is saying since count has been assigned to the value 0, add count with the value 1.
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
+// Describe what line 3 is doing, in particular, focus on what = is doing
