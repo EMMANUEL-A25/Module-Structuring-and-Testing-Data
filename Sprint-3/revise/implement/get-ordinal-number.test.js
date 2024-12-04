3da5a951eb2a5aebafa1e954a8177cd7e1c0142e
Emmanuel <esiziba71@gmail.com>
2024-12-04 07:33:53 +0000

    get ornial number test cases

--- Sprint-3/revise/implement/get-ordinal-number.test.js
+++ Sprint-3/revise/implement/get-ordinal-number.test.js
@@ -2,3 +2,18 @@
 
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
+
+const getOrdinalNumber = require('./get-ordinal-number');
+
+test("converts 1 to an ordinal number", function () {
+  expect(getOrdinalNumber(1)).toEqual("1st");
+});
+
+test("converts 2 to an ordinal number", function () {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+});
+
+test("converts 3 to an ordinal number", function () {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+});
+ 
