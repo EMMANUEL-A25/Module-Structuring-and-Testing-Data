ce5e6fdaf6b682b7f0542648500e388f133fea91
Emmanuel <esiziba71@gmail.com>
2024-12-04 14:22:31 +0200

    Update 0.js

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -5,18 +5,23 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`)
 
-Preditction: the function is going to print the answer of 10 and 32. 
-          It is also going to print "The result of multiplyling 10and 32 is"
-The Output of the above code is : 320. The result of multiplying 10 and 32 is.
+Prediction: the function is going to print the answer to 10 and 32. 
+          It is also going to print "The result of multiplying 10 and 32 is"
+The Output of the above code is 320. The result of multiplying 10 and 32 is.
 
-The code above gives us 2 resutls and that not what we want to fix it we have to remove one console log()
+The code above gives us 2 results and that is not what we want to fix we have to remove one console log(). After moving one console log we need to edit our code by declaring 2 variables a and b and log them to print our output. 
 
-fixing the error: 
 
+Fixing the error: 
 function multiply(a, b) {
-  return (a * b) 
-}
-
-console.log('The result of multiplying 10 and 32 is ${multiply(10,32)}')
+    console.log(a * b);
+  }
+  
+  let a = 10;
+  let b = 32;
+ 
+  let result = multiply(a, b)
+  
+  console.log(`The result of multiplying ${a} and ${b} is ${result}`);
 
 
