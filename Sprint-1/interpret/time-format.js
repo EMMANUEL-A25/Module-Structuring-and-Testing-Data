3aedbf0a27e8752d1fbadcd69e01e97e4a4d1771
Emmanuel <esiziba71@gmail.com>
2024-12-04 12:06:41 +0200

    Update time-format.js

--- Sprint-1/interpret/time-format.js
+++ Sprint-1/interpret/time-format.js
@@ -1,10 +1,10 @@
 const movieLength = 8784; // length of movie in seconds
 
-const remainingSeconds = movieLength % 60;
-const totalMinutes = (movieLength - remainingSeconds) / 60;
+const remaining seconds = movie-length % 60;
+const total minutes = (movie length - remaining seconds) / 60;
 
-const remainingMinutes = totalMinutes % 60;
-const totalHours = (totalMinutes - remainingMinutes) / 60;
+const remaining minutes = total minutes % 60;
+const total hours = (total minutes - remaining minutes) / 60;
 
 const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
 console.log(result);
@@ -17,18 +17,18 @@ console.log(result);
 // b) How many function calls are there?
  The are no function calls
 
-// c) Using documentation, explain what the expression movieLength % 60 represents
-movieLength is a variable that was asigned the value 8764, so the expression represents the movie length in seconds divided by 60 
+// c) Using documentation, explain what the expression movie-length % 60 represents
+movie length is a variable that was assigned the value 8764, so the expression represents the movie length in seconds divided by 60 
 and also giving us the reminder of that calculation using the modulo operator(%)
 
 
 
-// d) Interpret line 4, what does the expression assigned to totalMinutes mean?
- As the  movieLength and remainingSeconds variabes were declared in seconds line 4 is converting them into minutes by dividing them by 60.
+// d) Interpret line 4, what does the expression assigned to total minutes mean?
+ As the  movieLength and remainingSeconds variables were declared in seconds line 4 is converting them into minutes by dividing them by 60.
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
-it represents the strings, another name  you can use is timestring
+it represents the strings, another name  you can use is the time string
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
-I beilieve so becuase the code allows us to breakdown the total number of seconds into totalHours, minutes and seconds and also allows to get accurate results for any given movies length.
-The modulo operate also improves our code's efficiency  because we able to use the remainder of any value we punch in 
+I believe so because the code allows us to break down the total number of seconds into total hours, minutes, and seconds and also allows us to get accurate results for any given movie's length.
+The modulo operation also improves our code's efficiency  because we can use the remainder of any value we punch in 
