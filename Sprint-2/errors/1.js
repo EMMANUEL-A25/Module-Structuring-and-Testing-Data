4f287021969c863980b92afc503c24f518a94283
Emmanuel <esiziba71@gmail.com>
2024-12-06 12:50:16 +0200

    Update 1.js

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -12,20 +12,16 @@ function convertToPercentage(decimalNumber) {
 
 console.log(decimalNumber);
 
-The error occured because the decimal number variable that was declared, 
-had already been declared in the function input. 
-Also we got the worng reuslts becuase our decimalNumber = 0.5 variable was inside the function instead of outside
-to fix  this heres is the correct code
-
-const decimalNumber = 0.5;
- function convertToPercentage(decimalNumber) {
-
+//There is something wrong with our syntax. What is happening is that the expression in our function parameters shares the same name as the variable declared inside of the function. That is why we are getting an error
+//Fixed solution
+ function convertToPercentage(decimal) {
+  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
-
-console.log(decimalNumber);
+let result = convertToPercentage();
+console.log(result);
 
 
 
