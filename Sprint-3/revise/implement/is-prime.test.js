3603ef86ccbed0e04ba316d42a3e76b16ded3a67
Emmanuel <esiziba71@gmail.com>
2024-12-09 15:15:32 +0200

    Update is-prime.test.js

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -6,4 +6,12 @@ const isPrime = require('./is-prime');
 
 test('checks if 7 is a prime number', () => {
     expect(isPrime(7)).toBe(true); 
+    expect(isPrime(0)).toBe(false); 
+    expect(isPrime(1)).toBe(false);  
+    expect(isPrime(2)).toBe(true);  
+    expect(isPrime(3)).toBe(true);   
+    expect(isPrime(5)).toBe(true);  
+    expect(isPrime(6)).toBe(false);  
+    expect(isPrime(23)).toBe(true); 
+    expect(isPrime(24)).toBe(false); 
 });
