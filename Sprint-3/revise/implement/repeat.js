660da3efc4392af0b320307617bbb756125f8a02
Emmanuel <esiziba71@gmail.com>
2024-12-09 14:23:00 +0200

    Update repeat.js
    Spelling error

--- Sprint-3/revise/implement/repeat.js
+++ Sprint-3/revise/implement/repeat.js
@@ -1,6 +1,6 @@
 function repeat(str, count){
     if (count < 0) {
-        throw new Error("Count cannot be nagative");
+        throw new Error("Count cannot be negative");
     }
     let result = "";
     for (let i = 0; i < count; i++) {
