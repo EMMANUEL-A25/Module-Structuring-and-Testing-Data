293e59f4bff7f5b8965c050768888db0a95c084d
Emmanuel <esiziba71@gmail.com>
2024-12-09 15:20:26 +0200

    Update is-prime.js

--- Sprint-3/revise/implement/is-prime.js
+++ Sprint-3/revise/implement/is-prime.js
@@ -1,9 +1,8 @@
  function isPrime(num) {
-  // Handle base cases: 0, 1, and 2
+  
   if (num <= 1) return false;
   if (num === 2) return true;
-
-  // Check only odd numbers from 3 to the square root of num
+  
   const sqrtNum = Math.sqrt(num);
   for (let i = 3; i <= sqrtNum; i += 2) {
     if (num % i === 0) {
