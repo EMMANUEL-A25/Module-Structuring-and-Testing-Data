806bfcd0a3512781ab46eadd03eb46f47d94760e
Emmanuel <esiziba71@gmail.com>
2024-12-15 19:03:32 +0200

    Update is-proper-fraction.js

--- Sprint-3/implement/is-proper-fraction.js
+++ Sprint-3/implement/is-proper-fraction.js
@@ -33,13 +33,12 @@
 // Explanation: The fraction 3/3 is not a proper fraction because the numerator is equal to the denominator. The function should return false.
 // These acceptance criteria cover a range of scenarios to ensure that the isProperFraction function handles both proper and improper fractions correctly and handles potential errors such as a zero denominator.
 
-
-function isProperFraction(numerator, denominator){
-    if (denominator === 0) {
-      throw new Error("Denominator cannot be zero");
-    }
-    return numerator < denominator;
+ function isProperFraction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error("Denominator cannot be zero");
   }
+  return Math.abs(numerator) < denominator;
+}
 
-  module.exports = isProperFraction;
+module.exports = isProperFraction;
 
